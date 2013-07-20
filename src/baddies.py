5bdfd9e8c60ad4151e854b82f51b19715874d41f
Seth Polsley <scpolsley@gmail.com>
2013-07-20 14:10:13 -0500

    Added baddie random generation

--- src/baddies.py
+++ src/baddies.py
@@ -1,7 +1,24 @@
 # baddies.py
 
-def generateBaddies(char, map):
-	x, y = char.sprite.get_position()
+from random import randint
+from sprite import *
 
-	no_zone = Rect((x,y),(100,100))
+def generateBaddies(char, map):
+	noBaddies = map.copy()
+	playerSprite = pygame.sprite.Sprite()
+
+	x, y = char.sprite.get_position()
+	no_zone = pygame.rect.Rect((x,y),(100,100))
+	playerSprite.rect = no_zone
+	noBaddies.add(playerSprite)
+
+	baddies = pygame.sprite.Group()
+
+	for b in range(1,10):
+		posx, posy = random.randint(0,WIDTH), random.randint(0,HEIGHT)
+		b = Sprite('../resources/baddie.png')
+		b.set_position(posx, posy)
+		if spritecollideany(b, noBaddies) == None:
+			baddies.add(b)
+			b += 1
 
