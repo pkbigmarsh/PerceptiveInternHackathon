d63910eeb2657561fdc9150564ab6e0ca0da85a7
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-07-20 13:09:29 -0500

    basic character class

--- src/engine.py
+++ src/engine.py
@@ -6,6 +6,10 @@
 import pygame, sys
 from pygame.locals import *
 from sprite import *
+from character import *
+
+sys.path.append('./constants')
+from DIRECTIONS import *
 
 ## !! ----- Constants ----- !! ##
 FRAME_RATE = 30
@@ -18,25 +22,23 @@ fpsClock = pygame.time.Clock()
 windowSurfaceObj = pygame.display.set_mode(SCREEN_SIZE)
 pygame.display.set_caption('Hackathon')
 
-char = Sprite('../resources/char.png')
-char.rect.x = 0
-char.rect.y = 0
+char = Character()
 
 whiteColor = pygame.Color(255,255,255)
 
 while True:
 	windowSurfaceObj.fill(whiteColor)
-	
-	char.draw(windowSurfaceObj)
 
 	for event in pygame.event.get():
 		if event.type == QUIT:
 			pygame.quit()
 			sys.exit()
 		elif event.type == KEYDOWN:
-			print 'nothing here yet'
+			char.move(SOUTH)
 			#if event.key in (K_LEFT, K_RIGHT, K_UP, K_DOWN):
 				# TODO: move sprite
+
+	char.draw(windowSurfaceObj)
 
 	pygame.display.update()
 	fpsClock.tick(30)
--- src/sprite.py
+++ src/sprite.py
@@ -6,11 +6,15 @@ class Sprite(pygame.sprite.Sprite):
 		self.image = pygame.image.load(filepath)
 		self.rect = self.image.get_rect()
 
-	def set_(self, inc_x, inc_y):
-		self.rect.move((inc_x, inc_y))
+	def set_position(self, inc_x, inc_y):
+		self.rect.x = inc_x
+		self.rect.y = inc_y
 
 	def get_position(self):
 		return (self.rect.x, self.rect.y)
+
+	def move(self, x, y):
+		self.rect = self.rect.move(x, y)
 
 	def draw(self, window_surface):
 		window_surface.blit(self.image, self.rect)
