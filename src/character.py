6caa8769aad4bda929dbcdb85667d40595c7fa9c
Sarah Withee <sarah@sarahwithee.com>
2013-07-20 16:44:57 -0500

    Fixed problems with movement and keyboard control

--- src/character.py
+++ src/character.py
@@ -6,27 +6,29 @@ class Character:
 	def __init__(self):
 		self.sprite = Sprite('../resources/char.png')
 		self.health = 10
-		self.speed = 20
+		self.speed = 40
 
 	def move(self, direction):
 		#pos = self.sprite.getPosition()
+		charWidth = self.sprite.rect.width
+		charHeight = self.sprite.rect.height
 		curX = self.sprite.rect.x
 		curY = self.sprite.rect.y
-		moveX = curX + (direction[0] * self.speed)
-		moveY = curY + (direction[1] * self.speed)
+		newX = curX + (direction[0] * self.speed)
+		newY = curY + (direction[1] * self.speed)
 
 		# Detect if near any edges/walls
-		if moveX <= 0:
-			moveX = 0
-		elif moveX >= WIDTH - self.sprite.rect.width:
-			moveX = WIDTH - self.sprite.rect.width
+		if newX <= 0:
+			newX = 0 
+		elif newX >= WIDTH - charWidth:
+			newX = WIDTH - charWidth
 
-		if moveX <= 0:
-			moveX = 0
-		elif moveX >= WIDTH - self.sprite.rect.height:
-			moveX = WIDTH - self.sprite.rect.height
+		if newY <= 0:
+			newY = 0 
+		elif newY >= HEIGHT - charHeight:
+			newY = HEIGHT - charHeight
 
-		self.sprite.set_position(moveX, moveY)
+		self.sprite.set_position(newX, newY)
 
 	def draw(self, window_surface):
 		self.sprite.draw(window_surface)
