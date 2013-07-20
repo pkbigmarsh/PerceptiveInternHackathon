dc5860c6e498ed73b6514507966d8a0e335f1a61
Marshall Sumwalt <marshallsumwalt@gmail.com>
2013-07-20 12:04:40 -0500

    added the basic sprit class

--- src/main.py
+++ src/main.py
@@ -2,6 +2,8 @@
 import pygame, sys
 from pygame.locals import *
 
+import ./constants/COLORS
+
 ## !! ----- Constants ----- !! ##
 FRAME_RATE = 30
 SCREEN_SIZE = WIDTH, HEIGHT = 800, 400
@@ -13,3 +15,16 @@ SCREEN_SIZE = WIDTH, HEIGHT = 800, 400
 pygame.init()
 windowSurfaceObj = pygame.display.set_mode(SCREEN_SIZE)
 
+
+## !! ------ Main Loop ----- !! ##
+while True:
+	windowSurfaceObj.fill(WHITE)
+
+	## !! ----- Event Handling ----- !! ##
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
+	fpsClock.tick(FRAME_RATE)
